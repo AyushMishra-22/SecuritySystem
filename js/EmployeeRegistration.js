bd2254883b33efe38b84ec040bb28c1b54cbdab5
Bhumig02 <bhumigupta0707@gmail.com>
2023-09-30 00:03:50 +0530

    added css and js

--- js/EmployeeRegistration.js
+++ js/EmployeeRegistration.js
@@ -1,24 +1,3 @@
-{/* <script>
-    // Get the password and confirm password fields
-    var passwordField = document.getElementById('employee-password');
-    var confirmPasswordField = document.getElementById('employee-password-confirm');
-
-    // Check if the password and confirm password fields match
-    function validateForm() {
-      if (passwordField.value !== confirmPasswordField.value) {
-        alert('The password and confirm password fields do not match.');
-        return false;
-      }
-
-      // Continue with form submission
-      return true;
-    }
-
-    // Add a submit event listener to the form
-    document.querySelector('form').addEventListener('submit', validateForm);
-  </script> */}
-
-//   Include all the EmployeeRegistration.css Here 
 function validateForm() {
   var employeeId = document.getElementById("employee-id").value;
   var employeeName = document.getElementById("employee-name").value;
--- js/StudentRegistration.js
+++ js/StudentRegistration.js
@@ -1,46 +1,28 @@
-// Include all the neccessary checks for StudentRegistration.html
-// Get the password and confirm password fields
-// var passwordField = document.getElementById('student-password');
-// var confirmPasswordField = document.getElementById('student-password-confirm');
-
-// // Check if the password and confirm password fields match
-// if (passwordField.value !== confirmPasswordField.value) {
-//   // Display an error message
-//   alert('The password and confirm password fields do not match.');
-
-//   // Prevent the form from being submitted
-//   return false;
-// }
-
-// // Continue with form submission
-// return true;
-
 function validateForm() {
-    var studentName = document.getElementById("student-name").value;
-    var studentEmail = document.getElementById("student-email").value;
-    var studentPassword = document.getElementById("student-password").value;
-    var confirmPassword = document.getElementById("student-password-confirm").value;
-  
-    // Check if required fields are empty
-    if (studentName === "" || studentEmail === "" || studentPassword === "" || confirmPassword === "") {
-      alert("All fields are required.");
-      return false; // Prevent form submission
-    }
-  
-    // Check if the password and confirm password fields match
-    if (studentPassword !== confirmPassword) {
-      alert("Passwords do not match.");
-      return false; // Prevent form submission
-    }
-  
-    // Email validation using regex
-    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(studentEmail)) {
-      alert("Please enter a valid email address.");
-      return false; // Prevent form submission
-    }
-  
-    // If all checks pass, allow form submission
-    return true;
+  var studentName = document.getElementById("student-name").value;
+  var studentEmail = document.getElementById("student-email").value;
+  var studentPassword = document.getElementById("student-password").value;
+  var confirmPassword = document.getElementById("student-password-confirm").value;
+
+  // Check if required fields are empty
+  if (studentName === "" || studentEmail === "" || studentPassword === "" || confirmPassword === "") {
+    alert("All fields are required.");
+    return false; // Prevent form submission
   }
-  
+
+  // Check if the password and confirm password fields match
+  if (studentPassword !== confirmPassword) {
+    alert("Passwords do not match.");
+    return false; // Prevent form submission
+  }
+
+  // Email validation using regex
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(studentEmail)) {
+    alert("Please enter a valid email address.");
+    return false; // Prevent form submission
+  }
+
+  // If all checks pass, allow form submission
+  return true;
+}
